d0210d5cd392e2f585b9655d0e120ce54609e1d5
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-06-17 21:24:12 +0530

    cleaned card flip function

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const cards = document.querySelectorAll(".card");
 
 cards.forEach(card => {
   card.addEventListener("click", function(e) {
-    console.log(e.target.parent);
     e.target.parentElement.classList.toggle("flipped");
   });
 });
