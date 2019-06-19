444ef80849a7869bb6b07e698329a286dd9beb6c
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-06-19 17:23:21 +0530

    populated each box section using Javascript.

--- src/index.js
+++ src/index.js
@@ -1,58 +1,59 @@
 import "./styles.css";
 
-const cards = document.querySelectorAll(".card");
-const backElems = document.querySelectorAll(".back");
+// const cards = document.querySelectorAll(".card");
+// const backElems = document.querySelectorAll(".back");
 
-const imagesArray = [
-  `<i class="fab fa-twitter-square"></i>`,
-  `<i class="fab fa-facebook-square"></i>`,
-  `<i class="fab fa-linkedin"></i>`,
-  `<i class="fab fa-telegram"></i>`,
-  `<i class="fab fa-instagram"></i>`,
-  `<i class="fab fa-discord"></i>`,
-  `<i class="fab fa-snapchat-square"></i>`,
-  `<i class="fab fa-whatsapp-square"></i>`
-];
-let backElemsArray = Array.from(backElems);
-//console.log(backElemsArray)
-const backImages = [...imagesArray, ...imagesArray];
+// const imagesArray = [
+//   `<i class="fab fa-twitter-square"></i>`,
+//   `<i class="fab fa-facebook-square"></i>`,
+//   `<i class="fab fa-linkedin"></i>`,
+//   `<i class="fab fa-telegram"></i>`,
+//   `<i class="fab fa-instagram"></i>`,
+//   `<i class="fab fa-discord"></i>`,
+//   `<i class="fab fa-snapchat-square"></i>`,
+//   `<i class="fab fa-whatsapp-square"></i>`
+// ];
+// let backElemsArray = Array.from(backElems);
+// //console.log(backElemsArray)
+// const backImages = [...imagesArray, ...imagesArray];
 
-backImages.forEach(image => {
-  let randomCard = Math.floor(Math.random() * backElemsArray.length);
-  backElemsArray[randomCard].innerHTML = image;
-  backElemsArray.splice(randomCard, 1);
-});
+// backImages.forEach(image => {
+//   let randomCard = Math.floor(Math.random() * backElemsArray.length);
+//   backElemsArray[randomCard].innerHTML = image;
+//   backElemsArray.splice(randomCard, 1);
+// });
 
-function startGame() {
-  let matchingArray = [];
-  let matchedArray = [];
-  cards.forEach(card => {
-    card.addEventListener("click", function(e) {
-      e.target.parentElement.className += " flipped";
-      setTimeout(() => {
-        matchingArray.push(e.target.nextElementSibling);
-        if (matchingArray.length === 2) {
-          if (matchingArray[0].innerHTML === matchingArray[1].innerHTML) {
-            matchedArray = [...matchedArray, ...matchingArray];
-            matchingArray = [];
-            if (matchedArray.length === 16) {
-              alert("you won!");
-            }
-          } else {
-            matchingArray.forEach(elem => {
-              elem.parentElement.classList.remove("flipped");
-            });
-            matchingArray = [];
-          }
-        }
-      }, 1000);
-    });
-  });
-}
+// function startGame() {
+//   let matchingArray = [];
+//   let matchedArray = [];
+//   cards.forEach(card => {
+//     card.addEventListener("click", function(e) {
+//       e.target.parentElement.className += " flipped";
+//       setTimeout(() => {
+//         matchingArray.push(e.target.nextElementSibling);
+//         if (matchingArray.length === 2) {
+//           if (matchingArray[0].innerHTML === matchingArray[1].innerHTML) {
+//             matchedArray = [...matchedArray, ...matchingArray];
+//             matchingArray = [];
+//             if (matchedArray.length === 16) {
+//               alert("you won!");
+//             }
+//           } else {
+//             matchingArray.forEach(elem => {
+//               elem.parentElement.classList.remove("flipped");
+//             });
+//             matchingArray = [];
+//           }
+//         }
+//       }, 1000);
+//     });
+//   });
+// }
 
-startGame();
+// startGame();
 
 function buildMatchingGame() {
+  const gameDiv = document.querySelector(".matching-game");
   const cards = document.querySelectorAll(".card");
   const backElems = document.querySelectorAll(".back");
   const imagesArray = [
@@ -67,15 +68,38 @@ function buildMatchingGame() {
   ];
   let backElemsArray = Array.from(backElems);
   const backImages = [...imagesArray, ...imagesArray];
+  const matchingGame = {
+    createBox: () => {
+      const matchBox = document.createElement("section");
+      const cardDiv = document.createElement("div");
+      const front = document.createElement("div");
+      const back = document.createElement("div");
+
+      matchBox.classList.add("match-box");
+      cardDiv.classList.add("card");
+      front.classList.add("front");
+      back.classList.add("back");
+
+      gameDiv.appendChild(matchBox);
+      matchBox.appendChild(cardDiv);
+      cardDiv.appendChild(front);
+      cardDiv.appendChild(back);
+    },
+    populateBoxes: () => {
+      for (let i = 0; i < 16; i++) {
+        matchingGame.createBox();
+      }
+    },
+    addImages: () => {
+      backImages.forEach(image => {
+        let randomCard = Math.floor(Math.random() * backElemsArray.length);
+        backElemsArray[randomCard].innerHTML = image;
+        backElemsArray.splice(randomCard, 1);
+      });
+    }
+  };
+  matchingGame.populateBoxes();
+  //matchingGame.addImages();
 }
-const matchingGame = {
-  populateBoxes: () => {},
-  addImages: () => {
-    backImages.forEach(image => {
-      let randomCard = Math.floor(Math.random() * backElemsArray.length);
-      backElemsArray[randomCard].innerHTML = image;
-      backElemsArray.splice(randomCard, 1);
-    });
-  }
-};
-buildMatchingGame();
+
+const matchingGame = new buildMatchingGame();
