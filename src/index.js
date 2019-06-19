9b77c99a4329c168cfe14ba816998d6ef5811d57
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-06-19 18:16:08 +0530

    added reset button

--- src/index.js
+++ src/index.js
@@ -1,61 +1,8 @@
 import "./styles.css";
 
-// const cards = document.querySelectorAll(".card");
-// const backElems = document.querySelectorAll(".back");
-
-// const imagesArray = [
-//   `<i class="fab fa-twitter-square"></i>`,
-//   `<i class="fab fa-facebook-square"></i>`,
-//   `<i class="fab fa-linkedin"></i>`,
-//   `<i class="fab fa-telegram"></i>`,
-//   `<i class="fab fa-instagram"></i>`,
-//   `<i class="fab fa-discord"></i>`,
-//   `<i class="fab fa-snapchat-square"></i>`,
-//   `<i class="fab fa-whatsapp-square"></i>`
-// ];
-// let backElemsArray = Array.from(backElems);
-// //console.log(backElemsArray)
-// const backImages = [...imagesArray, ...imagesArray];
-
-// backImages.forEach(image => {
-//   let randomCard = Math.floor(Math.random() * backElemsArray.length);
-//   backElemsArray[randomCard].innerHTML = image;
-//   backElemsArray.splice(randomCard, 1);
-// });
-
-// function startGame() {
-//   let matchingArray = [];
-//   let matchedArray = [];
-//   cards.forEach(card => {
-//     card.addEventListener("click", function(e) {
-//       e.target.parentElement.className += " flipped";
-//       setTimeout(() => {
-//         matchingArray.push(e.target.nextElementSibling);
-//         if (matchingArray.length === 2) {
-//           if (matchingArray[0].innerHTML === matchingArray[1].innerHTML) {
-//             matchedArray = [...matchedArray, ...matchingArray];
-//             matchingArray = [];
-//             if (matchedArray.length === 16) {
-//               alert("you won!");
-//             }
-//           } else {
-//             matchingArray.forEach(elem => {
-//               elem.parentElement.classList.remove("flipped");
-//             });
-//             matchingArray = [];
-//           }
-//         }
-//       }, 1000);
-//     });
-//   });
-// }
-
-// startGame();
-
 function buildMatchingGame() {
   const gameDiv = document.querySelector(".matching-game");
-  const cards = document.querySelectorAll(".card");
-  const backElems = document.querySelectorAll(".back");
+  const resetBtn = document.querySelector(".reset");
   const imagesArray = [
     `<i class="fab fa-twitter-square"></i>`,
     `<i class="fab fa-facebook-square"></i>`,
@@ -66,7 +13,6 @@ function buildMatchingGame() {
     `<i class="fab fa-snapchat-square"></i>`,
     `<i class="fab fa-whatsapp-square"></i>`
   ];
-  let backElemsArray = Array.from(backElems);
   const backImages = [...imagesArray, ...imagesArray];
   const matchingGame = {
     createBox: () => {
@@ -91,15 +37,52 @@ function buildMatchingGame() {
       }
     },
     addImages: () => {
+      const backElems = document.querySelectorAll(".back");
+      let backElemsArray = Array.from(backElems);
       backImages.forEach(image => {
         let randomCard = Math.floor(Math.random() * backElemsArray.length);
         backElemsArray[randomCard].innerHTML = image;
         backElemsArray.splice(randomCard, 1);
       });
+    },
+    startGame: () => {
+      let matchingArray = [];
+      let matchedArray = [];
+      const cards = document.querySelectorAll(".card");
+      cards.forEach(card => {
+        card.addEventListener("click", function(e) {
+          e.target.parentElement.className += " flipped";
+          setTimeout(() => {
+            matchingArray.push(e.target.nextElementSibling);
+            if (matchingArray.length === 2) {
+              if (matchingArray[0].innerHTML === matchingArray[1].innerHTML) {
+                matchedArray = [...matchedArray, ...matchingArray];
+                matchingArray = [];
+                if (matchedArray.length === 16) {
+                  alert("you won!");
+                }
+              } else {
+                matchingArray.forEach(elem => {
+                  elem.parentElement.classList.remove("flipped");
+                });
+                matchingArray = [];
+              }
+            }
+          }, 1000);
+        });
+      });
+    },
+    resetGame: e => {
+      const cards = document.querySelectorAll(".card");
+      cards.forEach(card => {
+        card.parentElement.classList.remove("flipped");
+      });
     }
   };
   matchingGame.populateBoxes();
-  //matchingGame.addImages();
+  matchingGame.addImages();
+  matchingGame.startGame();
+  resetBtn.addEventListener("click", matchingGame.resetGame);
 }
 
 const matchingGame = new buildMatchingGame();
