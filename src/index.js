527c7342752bbd270c2613902afab93282d31fbc
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-06-19 15:26:13 +0530

    refactoring the app

--- src/index.js
+++ src/index.js
@@ -51,3 +51,31 @@ function startGame() {
 }
 
 startGame();
+
+function buildMatchingGame() {
+  const cards = document.querySelectorAll(".card");
+  const backElems = document.querySelectorAll(".back");
+  const imagesArray = [
+    `<i class="fab fa-twitter-square"></i>`,
+    `<i class="fab fa-facebook-square"></i>`,
+    `<i class="fab fa-linkedin"></i>`,
+    `<i class="fab fa-telegram"></i>`,
+    `<i class="fab fa-instagram"></i>`,
+    `<i class="fab fa-discord"></i>`,
+    `<i class="fab fa-snapchat-square"></i>`,
+    `<i class="fab fa-whatsapp-square"></i>`
+  ];
+  let backElemsArray = Array.from(backElems);
+  const backImages = [...imagesArray, ...imagesArray];
+}
+const matchingGame = {
+  populateBoxes: () => {},
+  addImages: () => {
+    backImages.forEach(image => {
+      let randomCard = Math.floor(Math.random() * backElemsArray.length);
+      backElemsArray[randomCard].innerHTML = image;
+      backElemsArray.splice(randomCard, 1);
+    });
+  }
+};
+buildMatchingGame();
