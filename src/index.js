faa6fc138f5bd31b072ab88b5d49ab4a8561342d
Rahul Nayak <pragyes31@users.noreply.github.com>
2019-06-20 11:24:37 +0530

    refactoring css

--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ function buildMatchingGame() {
   const resetBtn = document.querySelector(".reset");
   const countDiv = document.querySelector(".count");
   const imagesArray = [
-    `<i class="fab fa-twitter-square"></i>`,
-    `<i class="fab fa-facebook-square"></i>`,
-    `<i class="fab fa-linkedin"></i>`,
-    `<i class="fab fa-telegram"></i>`,
-    `<i class="fab fa-instagram"></i>`,
-    `<i class="fab fa-discord"></i>`,
-    `<i class="fab fa-snapchat-square"></i>`,
-    `<i class="fab fa-whatsapp-square"></i>`
+    `<i class="fab fa-twitter-square fa-3x"></i>`,
+    `<i class="fab fa-facebook-square fa-3x"></i>`,
+    `<i class="fab fa-linkedin fa-3x"></i>`,
+    `<i class="fab fa-telegram fa-3x"></i>`,
+    `<i class="fab fa-instagram fa-3x"></i>`,
+    `<i class="fab fa-discord fa-3x"></i>`,
+    `<i class="fab fa-snapchat-square fa-3x"></i>`,
+    `<i class="fab fa-whatsapp-square fa-3x"></i>`
   ];
   const backImages = [...imagesArray, ...imagesArray];
   let count = 0;
